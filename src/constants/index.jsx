0722d41b8befcefac97e7b925dc99a2e92f2115e
bsquiroz <bemq1605@gmail.com>
2023-10-26 15:12:11 -0500

    add new skill

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,34 +1,39 @@
 export const colorsPrimary = [
-    "#47a992",
-    "#068DA9",
-    "#E57C23",
-    "#ACBCFF",
-    "#BE5A83",
-    "#917FB3",
-    "#3C486B",
-    "#D14D72",
-    "#F15A59",
+	"#47a992",
+	"#068DA9",
+	"#E57C23",
+	"#ACBCFF",
+	"#BE5A83",
+	"#917FB3",
+	"#3C486B",
+	"#D14D72",
+	"#F15A59",
 ];
 
 export const skills = [
-    {
-        id: 1,
-        name: "JavaScript",
-        icon: <i className="bx bxl-javascript"></i>,
-    },
-    {
-        id: 2,
-        name: "TypeScript",
-        icon: <i className="bx bxl-typescript"></i>,
-    },
-    {
-        id: 3,
-        name: "React",
-        icon: <i className="bx bxl-react"></i>,
-    },
-    {
-        id: 4,
-        name: "Node",
-        icon: <i className="bx bxl-nodejs"></i>,
-    },
+	{
+		id: 1,
+		name: "JavaScript",
+		icon: <i className="bx bxl-javascript"></i>,
+	},
+	{
+		id: 2,
+		name: "TypeScript",
+		icon: <i className="bx bxl-typescript"></i>,
+	},
+	{
+		id: 3,
+		name: "React",
+		icon: <i className="bx bxl-react"></i>,
+	},
+	{
+		id: 4,
+		name: "React native",
+		icon: <i className="bx bxl-react"></i>,
+	},
+	{
+		id: 5,
+		name: "Node",
+		icon: <i className="bx bxl-nodejs"></i>,
+	},
 ];
